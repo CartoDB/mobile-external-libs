2fc7c43c0c80d03ba1851fffb79564238939a9fe
mtehver <mark.tehver@gmail.com>
2016-09-25 20:05:35 +0300

    Removed accidentally committed files

--- libs-external/valhalla/midgard-1.1.2/scripts/dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-export LD_LIBRARY_PATH=.:`cat /etc/ld.so.conf.d/* | grep -v -E "#" | tr "\\n" ":" | sed -e "s/:$//g"`
-sudo apt-get install -y autoconf automake pkg-config libtool make pkg-config gcc g++ lcov
