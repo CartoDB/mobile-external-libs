73b390e9f5a47af4b03d2c0eee5c5af126d564ad
mtehver <mark.tehver@gmail.com>
2020-01-30 13:58:53 +0200

    Another hackfix for Valhalla 3 to fix its own hack regarding 'noexcept' usage

--- valhalla/include/valhalla/baldr/rapidjson_utils.h
+++ valhalla/include/valhalla/baldr/rapidjson_utils.h
@@ -16,6 +16,9 @@
 #define RAPIDJSON_ASSERT(x)                                                                          \
   if (!(x))                                                                                          \
   throw std::logic_error(RAPIDJSON_STRINGIFY(x))
+// CARTOHACK
+#undef RAPIDJSON_HAS_CXX11_NOEXCEPT
+#define RAPIDJSON_HAS_CXX11_NOEXCEPT 0
 
 #include <rapidjson/document.h>
 #include <rapidjson/error/en.h>
