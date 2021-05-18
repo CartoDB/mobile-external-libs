75da5acf316db8df4166c2799475a4dd77086e67
mtehver <mark.tehver@gmail.com>
2021-05-18 10:57:51 +0300

    Sqlite related cleanup

--- sqlite3pp/sqlite3ppext.cpp
+++ sqlite3pp/sqlite3ppext.cpp
@@ -174,17 +174,6 @@ namespace sqlite3pp
       fh_[name] = pfunction_base(new function_handler(h));
       return sqlite3_create_function(db_, name, nargs, SQLITE_UTF8, fh_[name].get(), function_impl, 0, 0);
     }
-
-    aggregate::aggregate(database& db) : db_(db.db_)
-    {
-    }
-
-    int aggregate::create(char const* name, function_handler s, function_handler f, int nargs)
-    {
-      ah_[name] = std::make_pair(pfunction_base(new function_handler(s)), pfunction_base(new function_handler(f)));
-      return sqlite3_create_function(db_, name, nargs, SQLITE_UTF8, &ah_[name], 0, step_impl, finalize_impl);
-    }
-
   } // namespace ext
 
 } // namespace sqlite3pp
